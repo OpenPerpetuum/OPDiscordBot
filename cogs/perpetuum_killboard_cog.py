5213cd77d73838856cf01b60f9146af4acf57a74
Veritania <Jesper_ant@hotmail.com>
2021-03-16 16:17:06 +0100

    Handle if kill would exceed field limit
    Barf commit, will be cleaned up later
    It contains a fair amount of debugging, and it fetches a STATIC kill to force simulate receiving a kill that would result in crossing the Field Limit = 25.
    
    Comitting to make sure I have a working state

--- cogs/perpetuum_killboard_cog.py
+++ cogs/perpetuum_killboard_cog.py
@@ -7,6 +7,7 @@ import discord
 from time import strptime
 from datetime import datetime, timedelta
 
+# region Setup
 API_URL = 'https://api.openperpetuum.com/killboard/kill?order-by[0][type]=field&order-by[0][field]=' \
           'date&order-by[0][direction]=desc'
 
@@ -20,8 +21,9 @@ TIME_PARSE_STRING = '%Y-%m-%d %H:%M:%S'
 
 config_json = bot_functions.load_config(KILLBOARD_CONFIG)
 bot_name_lookup = bot_functions.load_config(BOT_NAME_FILE)
+# endregion Setup
 
-# Defaults
+# region Defaults
 
 if 'killmail_channel' not in config_json:
     config_json['killmail_channel'] = 'op-general'
@@ -32,6 +34,9 @@ if 'update_interval_seconds' not in config_json:
     print("Killmail update interval not found in config file! Defaulting to 300 seconds.")
 
 
+# endregion Defaults
+
+# region Helper functions
 def get_last_kill_time():
     """Returns a struct_time object with the last saved kill. """
     global config_json  # Update the JSON in memory while we're reading the last time.
@@ -48,6 +53,52 @@ def prettier_numbers(number: int):
     pretty = x.split('.')
     return pretty[0]
 
+
+def add_attacker(embed: discord.embeds, a: json):
+    if a["hasKillingBlow"]:
+        embed.add_field(name="⚔ Attacker - 🩸 Killing Blow! 🩸",
+                        value=a['_embedded']['agent']['name'] +
+                              "\n**Corp**: " + a['_embedded']['corporation']['name'],
+                        inline=False)
+    else:
+        embed.add_field(name="⚔ Attacker",
+                        value=a['_embedded']['agent']['name'] +
+                              "\n**Corp**: " + a['_embedded']['corporation']['name'],
+                        inline=False)
+
+    embed.add_field(name="🤖 Robot",
+                    value=bot_name_lookup.get(a['_embedded']['robot']['definition']),
+                    inline=True)
+
+    embed.add_field(name="🗡️ Damage dealt",
+                    value=prettier_numbers(a['damageDealt']),
+                    inline=True)
+
+    if int(a['totalEcmAttempts']) > 0:
+        embed.add_field(name="ECM Attempts",
+                        value=a['totalEcmAttempts'],
+                        inline=True)
+
+    if int(a['sensorSuppressions']) > 0:
+        embed.add_field(name="Sensor Supressions",
+                        value=a['sensorSuppressions'],
+                        inline=True)
+
+    if float(a['energyDispersed']) > 0:
+        embed.add_field(name="Energy Drained",
+                        value=prettier_numbers(a['energyDispersed']),
+                        inline=True)
+    return embed
+
+
+def add_too_many_attackers(embed: discord.embeds):
+    embed.add_field(name="Full Details",
+                    value="Too many attackers to list, see the full details at the Killboard Link",
+                    inline=False)
+    return embed
+
+
+# endregion Helper functions
 
 class Killboard(commands.Cog):
     """Prototype Perpetuum killboard cog"""
@@ -110,13 +161,17 @@ class Killboard(commands.Cog):
 
         # Iterate over each new killmail
         for kill in new_killmails:
+            # Test
+            ijson = requests.get('https://api.openperpetuum.com/killboard/kill/2720')
+            kill = json.loads(ijson.content)
+
             # Embed Setup
             kill_message_embed = discord.Embed(title="Killboard Link",
                                                url="https://killboard.openperpetuum.com/kill/" + str(kill['id']),
                                                color=discord.colour.Color.random())
             kill_message_embed.set_author(name="Killmail #" + str(kill['id']),
                                           url="https://api.openperpetuum.com/killboard/kill/" + str(kill['id']),
-                                          icon_url="http://clipart-library.com/img/831510.png") # TODO: Decide on icon
+                                          icon_url="http://clipart-library.com/img/831510.png")  # TODO: Decide on icon
             # kill_message_embed.set_thumbnail(
             #     url="http://clipart-library.com/img/831510.png")  # TODO: Fetch Victims robot picture, or Corp icons?
 
@@ -141,48 +196,113 @@ class Killboard(commands.Cog):
                                          inline=True)
 
             # Embed - Attacker(s)
-            #TODO: Embeds has a hard-limit on 25 fields
+            # TODO: Embeds has a hard-limit on 25 fields
             # so if there is too many attackers or too many who did Drain/Supress/ECM
             # We need to shorten the list of attackers and put something like "... More attackers, check killboard link"
             # An example of this is with KillID: 2720
             # More info on embed limits: https://discordjs.guide/popular-topics/embeds.html#embed-limits
+
+            field_overflow: bool = False
+            field_count: int = 4  # We always start with 4 fields from the Victim section
+
+            # Count the total amount of fields this Killmail would require
             for a in kill['_embedded']['attackers']:
-
-                if a["hasKillingBlow"]:
-                    kill_message_embed.add_field(name="⚔ Attacker - 🩸 Killing Blow! 🩸",
-                                                 value=a['_embedded']['agent']['name'] +
-                                                       "\n**Corp**: " + a['_embedded']['corporation']['name'],
-                                                 inline=False)
-                else:
-                    kill_message_embed.add_field(name="⚔ Attacker",
-                                                 value=a['_embedded']['agent']['name'] +
-                                                       "\n**Corp**: " + a['_embedded']['corporation']['name'],
-                                                 inline=False)
-
-                kill_message_embed.add_field(name="🤖 Robot",
-                                             value=bot_name_lookup.get(a['_embedded']['robot']['definition']),
-                                             inline=True)
-
-                kill_message_embed.add_field(name="🗡️ Damage dealt",
-                                             value=prettier_numbers(a['damageDealt']),
-                                             inline=True)
-
+                print("Field Check Start - " + str(field_count))
+                field_count += 3  # At least we add 3 (Name, Robot, Damage as minimum)
                 if int(a['totalEcmAttempts']) > 0:
-                    kill_message_embed.add_field(name="ECM Attempts",
-                                                 value=a['totalEcmAttempts'],
-                                                 inline=True)
-
+                    field_count += 1
                 if int(a['sensorSuppressions']) > 0:
-                    kill_message_embed.add_field(name="Sensor Supressions",
-                                                 value=a['sensorSuppressions'],
-                                                 inline=True)
-
+                    field_count += 1
                 if float(a['energyDispersed']) > 0:
-                    kill_message_embed.add_field(name="Energy Drained",
-                                                 value=prettier_numbers(a['energyDispersed']),
-                                                 inline=True)
+                    field_count += 1
+                print("Field Check End: - " + str(field_count))
+            if (field_count > 25):
+                field_overflow = True
+                print("Found an Overflowing Kill ID: " + str(kill['id']))
+                field_count = 4  # Reset value to reuse while building overflow embed
 
+            # Do condensed attacker list, if Embed Field Count would go above the 25 MAX limit
+            if (field_overflow):
+                print("Entered FIELD OVERFLOW")
+
+                # Find Killing Blow and put them first on the embed
+                for a in kill['_embedded']['attackers']:
+                    if a["hasKillingBlow"]:
+                        add_attacker(kill_message_embed, a)
+                        field_count += 3  # At least we add 3 (Name, Robot, Damage as minimum)
+                        if int(a['totalEcmAttempts']) > 0:
+                            field_count += 1
+
+                        if int(a['sensorSuppressions']) > 0:
+                            field_count += 1
+
+                        if float(a['energyDispersed']) > 0:
+                            field_count += 1
+                        print("Added KB to embed, total fields: " + str(field_count))
+
+                # Used to check if we are trying to add the final attacker when we hit the MAX field count of 25
+                attacker_iteration: int = 1
+
+                # Build remaining attacker(s) embeds
+                for a in kill['_embedded']['attackers']:
+                    if a["hasKillingBlow"]:
+                        continue  # Do nothing, Killing Blow Attacker was already added earlier.
+                    else:
+                        # Count how many fields we want to add,
+                        # check against field_count to make sure it fits within <=25 max limit
+                        print("Attempting to add new attacker, Field count: " + str(field_count))
+                        fields_to_add: int = 3  # At least we add 3 (Name, Robot, Damage as minimum)
+                        if int(a['totalEcmAttempts']) > 0:
+                            fields_to_add += 1
+                        if int(a['sensorSuppressions']) > 0:
+                            fields_to_add += 1
+                        if float(a['energyDispersed']) > 0:
+                            fields_to_add += 1
+                        print("ATK Fields to add: " + str(fields_to_add))
+
+                        temp_field_total = fields_to_add + field_count  # Would-be total if we added the above fields
+
+                        print("Temp Total Fields: " + str(temp_field_total))
+
+                        # If we do not reach the Field Limit with this entry, then add this attacker to the embed
+                        if (temp_field_total <= 24):
+                            print("RES: Less than 24, adding..")
+                            add_attacker(kill_message_embed, a)
+                            field_count = temp_field_total  # Save that we added new fields
+                            print("NEW TOTAL: " + str(field_count))
+                            attacker_iteration += 1
+
+                        # If adding this entry would set our Field total = 25 (MAX) or less,
+                        # AND its the last attacker for the kill then we add it,
+                        # otherwise build the "More attackers... See Killboard for details"
+                        elif temp_field_total <= 25 & attacker_iteration == len(kill)-1:
+                            print("RES: <= 25 AND last entry, adding...")
+                            add_attacker(kill_message_embed, a)
+                            attacker_iteration += 1
+
+                        else:
+                            print("RES: OVERFLOW! Field Count atm: " + str(field_count))
+                            print("RES: OVERFLOW! Too many attackers msg adding")
+                            add_too_many_attackers(kill_message_embed)
+                            break
+            else:
+                # This killmail requires less than 25 fields, so go ahead and add all of them
+
+                # Find Killing Blow and put them first on the embed
+                for a in kill['_embedded']['attackers']:
+                    if a["hasKillingBlow"]:
+                        add_attacker(kill_message_embed, a)
+
+                for a in kill['_embedded']['attackers']:
+
+                    if a["hasKillingBlow"]:
+                        continue  # Already added the Killing Blow Entry
+                    else:
+                        add_attacker(kill_message_embed, a)
+
+            # Post the embed!
             for channel in channels:
+                print("-- POSTING EMBED MESSAGE --")
                 await channel.send(embed=kill_message_embed)
 
         return
